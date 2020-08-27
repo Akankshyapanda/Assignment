df9dd811fa9c3d8e5251793900ddaf48c18c64e0
Akankshyapanda <68200823+Akankshyapanda@users.noreply.github.com>
2020-08-27 19:47:04 +0530

    Add files via upload

---
+++ claci.js
@@ -0,0 +1,14 @@
+function add(){
+
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    alert(num1+num2);
+    }
+    function sub(){
+    
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    alert(num1-num2);
+    }
+    
+    
